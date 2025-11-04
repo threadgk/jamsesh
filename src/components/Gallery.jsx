cde03b3cc0e29079d206992e474c76e3de01e27f
Kayla Threadgill <krthread@gmail.com>
2025-11-04 12:13:48 -0500

    create users json

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,27 @@
-
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import './Gallery.css';
+import Page from './Page';
 const Gallery = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+
+    };
+    fetchUsers();
+  }, []);
+  
   return (
     <div className="gallery">
+      {users.map((user) => (
+        <Page 
+          key={user.id}
+          avatar={user.avatar}
+          name={user.name}
+        />
+      ))}
     </div>
   );
 };
