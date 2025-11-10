579d57413cf9b07d0d4a05fc69c41710d3ea8e61
Kayla Threadgill <krthread@gmail.com>
2025-11-09 21:12:06 -0500

    updated

--- src/components/Profile/Page.jsx
+++ src/components/Profile/Page.jsx
@@ -1,16 +1,16 @@
 import "/Users/kaylathreadgill/jamsesh/src/css/Profile.css";
 import "/Users/kaylathreadgill/jamsesh/src/css/Page.css";
-    const Page = (user) => {
+    const Page = ({banner, avatar, username, bio, location, favoriteArtist}) => {
         return (
             <div className="profile-component"> 
-                {user.banner && <img className="profile-banner" src={user.banner} alt="Profile Banner" />}
+                {banner && <img className="profile-banner" src={banner} alt="Profile Banner" />}
 
                 <div className="profile-info">
-                    <img className="profile-avatar" src={user.avatar} alt="Profile Avatar" />
-                    <h1 className="profile-username">{user.username}</h1>
-                    <p className="profile-bio">{user.bio}</p>
-                    <p className="profile-location">Location: {user.location}</p>
-                    <p className="profile-favorite-artist">Favorite Artist: {user.favoriteArtist}</p> 
+                    <img className="profile-avatar" src={avatar} alt="Profile Avatar" />
+                    <h1 className="profile-username">{username}</h1>
+                    <p className="profile-bio">{bio}</p>
+                    <p className="profile-location">Location: {location}</p>
+                    <p className="profile-favorite-artist">Favorite Artist: {favoriteArtist}</p> 
                 </div>
             </div>
         );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,31 @@
 import "./../css/Profile.css";
 import "./../css/Page.css";
-import Page from "/Users/kaylathreadgill/jamsesh/src/components/Profile/Page.jsx";
-const Profile = () => {
+import Page from "/Users/kaylathreadgill/jamsesh/src/components/Profile/Page.jsx"; 
+import { useState, useEffect } from "react";
+
+const Profile = () => { 
+    const [user, setUser] = useState(null); 
+
+    useEffect(() => { 
+        fetch("https://jamsesh-server-wcbm.onrender.com/api/artists")
+            .then(response => response.json())
+            .then(data => {setUser(data);})
+            .catch(error => {console.error("Error fetching user data:", error);});
+    }, []);
+
     return (
         <div>
             <h2>Profile Page</h2>
+            {user ? 
+            
             <Page 
-                avatar=""
-                username="kayla_threadgill"
-                bio="i am a music lover"
-                location="Columbia, SC"
-                favoriteArtist="Dominic Fike"
-            />
+                banner={user["Profile Picture"]}
+                avatar={user.avatar}
+                username={user._username}
+                bio={user._bio}
+                location={user._location}
+                favoriteArtist={user._favoriteArtist} 
+            /> : (<p>Loading...</p>)}
         </div>
     );
 };
