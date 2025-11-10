bc0c3b8c24c7e1b5f523e4d431b69190fc23fd7c
Kayla Threadgill <krthread@gmail.com>
2025-11-09 22:29:20 -0500

    adding modal pop up

--- src/components/Home/Slideshow.jsx
+++ src/components/Home/Slideshow.jsx
@@ -2,7 +2,9 @@ import "/Users/kaylathreadgill/jamsesh/src/css/Slideshow.css";
 import { useState } from "react";
 
 const Slideshow = () => {
-    const [slideIndex, setSlideIndex] = useState(0);
+    const [slideIndex, setSlideIndex] = useState(0);  
+    const [showModal, setShowModal] = useState(false);
+
 
     const importAll = (resource) => {
         return resource.keys().map(resource);
@@ -20,11 +22,24 @@ const images = importAll(
         return (
     <div id="slideshow-container">
         <h1>Page Preview</h1>
+
         <section className="slideshow">
-            <img src={images[slideIndex]} /> 
+            <img src={images[slideIndex]} 
+            alt = "Slideshow"
+            className = "slideshow-image" 
+            onClick = {() => setShowModal(true)}        
+            /> 
             <a className="prev" onClick={prevSlide} href="#prev">&#10094;</a>
             <a className="next" onClick={nextSlide} href="#next">&#10095;</a>
-        </section>
+        </section> 
+
+        {showModal && (
+            <div className="modal" onClick = {() => setShowModal(false)}>
+                <span className="close" onClick={() => setShowModal(false)}>&times;</span>
+                <img className="modal-content" src={images[slideIndex]} alt="Enlarged Slideshow" onClick = {(e) => e.stopPropagation()}/>
+                <div className="modal-caption">{`Image ${slideIndex + 1} of ${images.length}`}</div>
+            </div>
+        )}
     </div>
     );
 } 
